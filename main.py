7d9f8be0aef17aa63883575ab87aa1c454612d63
joeltancej <joeltancej@gmail.com>
2023-02-25 13:37:23 +0800

    home template and updating python code

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # main file
+
+from flask import Flask, render_template, url_for, request, redirect, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
